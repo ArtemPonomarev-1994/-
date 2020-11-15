adaf5fbe059c17b82d37fdc7fd7d8577a7107ae6
ArtemPoomarev-1994 <animal_jazz_1@mail.ru>
2020-11-15 14:32:34 +0400

    в

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ class App extends React.Component {
 
 <div className="container" id="cookie_info">
     <div className="site-content">
-        <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера.<br/>В
-            частности, для персонифицированной работы сайта мы обрабатываем IP-адрес региона вашего
-            местоположения.Шучу. Конечно, мы ничего не обрабатываем. Это же пет-проект.&nbsp;<button className="btn btn-primary btn-sm">OK</button></div>
+        <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера.<br/></div>
     </div>
 </div>
 
